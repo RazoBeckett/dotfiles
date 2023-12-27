4ab2e8bc9ef5193a40894f8de6a10cc75f9e2f11
Mayur Raut <rautmayur2517@gmail.com>
2023-12-28 00:10:52 +0530

    started nvim new, and modified some shellrcies

---
+++ .config/nvim/lua/plugins.lua
@@ -0,0 +1,8 @@
+return {
+ {
+    'nvim-telescope/telescope.nvim', tag = '0.1.5',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+}
+
