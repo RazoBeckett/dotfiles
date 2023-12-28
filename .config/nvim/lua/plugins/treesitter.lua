9b4c9692eadc8b032ab5438b1e3557d531b34fdf
Mayur Raut <rautmayur2517@gmail.com>
2023-12-28 13:46:48 +0530

    Progressing futher in neovim, added Xresources

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,11 +4,11 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "javascript", "bash", "python"},
+      auto_install = true,
       highlight = { enable = true },
-      indent = { enable = true },  
+      indent = { enable = true },
     })
-  end 
+  end
 }
 
 
