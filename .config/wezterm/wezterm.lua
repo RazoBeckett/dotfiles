951e6141d20a61a4caa8724023c8882cf07bb611
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2024-03-17 21:17:41 +0530

    add ffmpeg script & combine with topmenu

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,41 +1,40 @@
 local wezterm = require("wezterm")
-return {
-	color_scheme = "Catppuccin Macchiato",
-	enable_tab_bar = false,
-	font = wezterm.font_with_fallback({
-		{ family = "BlexMono Nerd Font", weight = "Regular" },
-		{ family = "ConkySymbols", weight = "Regular" },
-		"Noto Color Emoji",
-	}),
-	font_size = 12.0,
-	-- macos_window_background_blur = 40,
-	macos_window_background_blur = 30,
 
-	-- window_background_image = '/Users/omerhamerman/Downloads/3840x1080-Wallpaper-041.jpg',
-	-- window_background_image_hsb = {
-	-- 	brightness = 0.01,
-	-- 	hue = 1.0,
-	-- 	saturation = 0.5,
-	-- },
-	-- window_background_opacity = 0.92,
-	window_background_opacity = 0.7,
-	-- window_background_opacity = 0.78,
-	-- window_background_opacity = 0.20,
-	window_decorations = "RESIZE",
-	keys = {
-		{
-			key = "f",
-			mods = "CTRL",
-			action = wezterm.action.ToggleFullScreen,
-		},
-	},
-	cursor_blink_rate = 0, -- disable cursor blinking
-	mouse_bindings = {
-		-- Ctrl-click will open the link under the mouse cursor
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = wezterm.action.OpenLinkAtMouseCursor,
-		},
+local config = {}
+
+if wezterm.config_builder then
+	config = wezterm.config_builder()
+end
+
+config.color_scheme = "Catppuccin Macchiato"
+config.enable_tab_bar = false
+config.font = wezterm.font_with_fallback({
+	{ family = "BlexMono Nerd Font", weight = "Medium" },
+})
+config.font_size = 12.0
+config.macos_window_background_blur = 30
+config.window_background_opacity = 0.7
+config.window_decorations = "RESIZE"
+config.window_padding = {
+	top = "0.5cell",
+	bottom = "0.5cell",
+	left = "1.5cell",
+	right = "1.5cell",
+}
+config.keys = {
+	{
+		key = "f",
+		mods = "CTRL",
+		action = wezterm.action.ToggleFullScreen,
 	},
 }
+config.cursor_blink_rate = 0
+config.mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = wezterm.action.OpenLinkAtMouseCursor,
+	},
+}
+
+return config
