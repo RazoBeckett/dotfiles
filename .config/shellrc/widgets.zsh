58024d71decfdaf2e20e67fa5c74ea203e0ad7f0
Mayur Raut <95993675+RazoBeckett@users.noreply.github.com>
2025-07-04 15:21:53 +0530

    fix(zsh): push and accept scripts inputs
    - zle push-input
    - special `BUFFER` variable
    - zle accept-line

--- .config/shellrc/widgets.zsh
+++ .config/shellrc/widgets.zsh
@@ -1,13 +1,19 @@
 #!/usr/bin/env zsh
 
 # Widget for tmux sessionizer
-tmux_sessionizer_widget() { tmux-sessionizer }
+tmux_sessionizer_widget() {
+  zle push-input
+  BUFFER=tmux-sessionizer
+  zle accept-line
+}
 zle -N tmux_sessionizer_widget
 bindkey -M viins '^Bf' tmux_sessionizer_widget  # ctrl + f to open tmux sessionizer
 
 # Widget for sesh
 sesh_connect_widget() {
- sesh connect "$(sesh list -i | fzf --ansi --reverse --prompt=" " --pointer="" --ghost="Pick a sesh")"
+  zle push-input
+  BUFFER='sesh connect "$(sesh list -i | fzf --ansi --reverse --prompt=" " --pointer="" --ghost="Pick a sesh")"'
+  zle accept-line
 }
 zle -N sesh_connect_widget
 bindkey -M viins '^B]' sesh_connect_widget      # ctrl + ] to connect to a tmux session
